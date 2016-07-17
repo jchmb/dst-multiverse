194d40ce098ed19fcb05c0947a9e3572845c4150
jchmb <barelds.jochem@gmail.com>
2016-07-17 19:39:54 +0200

    modinfo

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 -- This information tells other players more about the mod
-name = "Snowpig"
-description = "Shard connections with migration portals."
-author = "Nuke (original author)"
+name = "000-Snowpig"
+description = "Multi-Worlds With Additional Stuff"
+author = "Joachim and Snook-8"
 version = "1.0.6"
 forumthread = ""
 api_version = 10	
@@ -12,6 +12,7 @@ icon = "modicon.tex"
 dst_compatible = true
 reign_of_giants_compatible = true
 
-server_only_mod = true
 all_clients_require_mod = true
 client_only_mod = false
+
+server_filter_tags = {"environment","worldgen"}
