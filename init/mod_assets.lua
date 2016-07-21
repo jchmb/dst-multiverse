88f0aa86c55bc8427ca5eae0b3f81241e6069628
jchmb <barelds.jochem@gmail.com>
2016-07-21 19:16:27 +0200

    Moved some stuff around

---
+++ init/mod_assets.lua
@@ -0,0 +1,41 @@
+Assets = {
+	-- Snow tiles
+	Asset("IMAGE", "levels/textures/noise_snowy.tex"),
+	Asset("IMAGE", "levels/textures/mini_noise_snowy.tex"),
+	Asset("IMAGE", "levels/tiles/snowy.tex"),
+	Asset("FILE", "levels/tiles/snowy.xml"),
+
+	-- Slimey tiles
+	Asset("IMAGE", "levels/textures/noise_slimey.tex"),
+	Asset("IMAGE", "levels/textures/mini_noise_slimey.tex"),
+	Asset("IMAGE", "levels/tiles/slimey.tex"),
+	Asset("FILE", "levels/tiles/slimey.xml"),
+
+	-- Assets for recipes
+	Asset("IMAGE", "images/inventoryimages/pighouse_yellow.tex" ),
+	Asset("ATLAS", "images/inventoryimages/pighouse_yellow.xml" ),
+	Asset("IMAGE", "images/inventoryimages/pighouse_blue.tex" ),
+	Asset("ATLAS", "images/inventoryimages/pighouse_blue.xml" ),
+	Asset("IMAGE", "images/inventoryimages/pighouse_gray.tex" ),
+	Asset("ATLAS", "images/inventoryimages/pighouse_gray.xml" ),
+
+	-- Items
+	Asset("IMAGE", "images/inventoryimages/dug_coffeebush.tex" ),
+	Asset("ATLAS", "images/inventoryimages/dug_coffeebush.xml" ),
+	Asset("IMAGE", "images/inventoryimages/coffeebeans.tex" ),
+	Asset("ATLAS", "images/inventoryimages/coffeebeans.xml" ),
+	Asset("IMAGE", "images/inventoryimages/coffeebeans_cooked.tex" ),
+	Asset("ATLAS", "images/inventoryimages/coffeebeans_cooked.xml" ),
+	Asset("IMAGE", "images/inventoryimages/coffee.tex" ),
+	Asset("ATLAS", "images/inventoryimages/coffee.xml" ),
+
+	-- Turfs
+	Asset("IMAGE", "images/inventoryimages/turf_snowy.tex" ),
+	Asset("ATLAS", "images/inventoryimages/turf_snowy.xml" ),
+	Asset("IMAGE", "images/inventoryimages/turf_slimey.tex" ),
+	Asset("ATLAS", "images/inventoryimages/turf_slimey.xml" ),
+
+	-- Minimap icons
+	Asset("MINIMAP_IMAGE", "teleportato"),
+	Asset("MINIMAP_IMAGE", "map_icons/minimap_coffeebush.tex"),
+}
