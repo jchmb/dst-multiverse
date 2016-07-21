c00a8a5704d4e109485010b7e20ee1709a6e7ccf
Jochem <jchmb@users.noreply.github.com>
2016-07-21 10:39:17 +0200

    Update utils.lua

--- utils.lua
+++ utils.lua
@@ -1,1 +1,16 @@
+function AddLevelFixed(level_type, data)
+    AddLevel(level_type, data)
+    data_copy = deepcopy(data)
+    data_copy.id = data.id .. "_NORMAL"
+    data_copy.location = "forest"
+    data_copy.overrides.taskset = data.overrides.taskset .. "_normal"
+    AddLevel(level_type, data_copy)
+end
 
+function AddTaskSetFixed(task_set, data)
+    AddTaskSet(task_set, data)
+    data_copy = deepcopy(data)
+    task_set_normal = task_set .. "_normal"
+    data_copy.location = "forest"
+    AddTaskset(task_set_normal, data_copy)
+end
