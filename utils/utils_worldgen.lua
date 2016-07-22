1a1ef5223fbf6554848b0939f354deeb1517c5ea
jchmb <barelds.jochem@gmail.com>
2016-07-22 09:03:24 +0200

    Commit

--- utils/utils_worldgen.lua
+++ utils/utils_worldgen.lua
@@ -1,23 +1,17 @@
-function AddTaskSetWrapped(task_set, data)
-   if GetModConfigData("UseMigrationPortals") then
-       data.set_pieces["MigrationGrass"] = data.set_pieces["CaveEntrance"]
-       data.set_pieces["CaveEntrance"] = nil
-   end
-end
-
 function AddLevelFixed(level_type, data)
-    AddLevel(level_type, data)
-    data_copy = deepcopy(data)
-    data_copy.id = data.id .. "_NORMAL"
-    data_copy.location = "forest"
-    data_copy.overrides.taskset = data.overrides.taskset .. "_normal"
-    AddLevel(level_type, data_copy)
+	-- if GetModConfigData("UseDefaultLocations") then
+	-- 	data.overrides.location = "forest"
+	-- end
+	AddLevel(level_type, data)
 end
 
 function AddTaskSetFixed(task_set, data)
-    AddTaskSetWrapped(task_set, data)
-    data_copy = deepcopy(data)
-    task_set_normal = task_set .. "_normal"
-    data_copy.location = "forest"
-    AddTaskSetWrapped(task_set_normal, data_copy)
+	--if GetModConfigData("UseMigrationPortals") then
+		data.set_pieces["MigrationGrass"] = data.set_pieces["CaveEntrance"]
+		data.set_pieces["CaveEntrance"] = nil
+	--end
+	-- if GetModConfigData("UseDefaultLocations") then
+	-- 	data.location = "forest"
+	-- end
+	AddTaskSet(task_set, data)
 end
