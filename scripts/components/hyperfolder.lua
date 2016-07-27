ee22049c7af134196a6a7af54f82505420013ddf
Jochem <jchmb@users.noreply.github.com>
2016-07-27 14:02:16 +0200

    Update hyperfolder.lua

--- scripts/components/hyperfolder.lua
+++ scripts/components/hyperfolder.lua
@@ -15,6 +15,42 @@ function Hyperfolder:Clear()
 	self.returning = false
 end
 
+function Hyperfolder:RegisterStartPortal(portal)
+	self.startWorldID = TheShard:GetID() -- TMP / TODO check if this is actually the right function
+	self.startPortalID = portal.id
+end
+
+function Hyperfolder:RegisterEndPortal(portal)
+	self.endWorldID = TheShard:GetID() -- TMP / TODO check if this is actually the right function
+	self.endPortalID = portal.id
+	portal.components.worldmigrator:SetReceivedPortal(self.startWorldID, self.startPortalID)
+end
+
+function Hyperfolder:DoReturn(portal, doer)
+	self.returning = true
+	portal.components.worldmigrator:Activate(doer)
+end
+
+function Hyperfolder:FindStartPortal()
+	if self.startPortalID ~= nil and self.startWorldID ~= nil then
+		for k,v in pairs(Ents) do
+			if v.components.worldmigrator ~= nil and v.components.worldmigrator.id == self.startPortalID then
+				return v	
+			end
+		end
+		return nil
+	else
+		self:Clear()
+		return nil
+	end
+end
+
+function Hyperfolder:OnReturn()
+	portal = self:FindStartPortal()
+	portal.components.worldmigrator:SetReceivedPortal(self.startWorldID, self.startPortalID)
+	self:Clear()
+end
+
 function Hyperfolder:OnLoad(data)
 	if data.startPortalID ~= nil and data.startWorldID ~= nil then
 		self.startPortalID = data.startPortalID
