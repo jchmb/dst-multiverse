da27d3b577ed0ad71bb05f4fb3c6d71354082ed9
Jochem <jchmb@users.noreply.github.com>
2016-07-27 15:41:25 +0200

    Update player_hooks.lua

--- init/player_hooks.lua
+++ init/player_hooks.lua
@@ -6,6 +6,27 @@ local onhit = function(inst, attacker, dmg)
     end
 end
 
+local HyperfolderOnBuild = function(builder, prod)
+	if prod.prefab == "migration_portal" then
+		builder.components.hyperfolder:OnBuild(prod)	
+	end
+end
+
+local AddHyperfolderComponent = function(player)
+	if player.components.hyperfolder ~= nil then
+		player:AddComponent("hyperfolder")
+		if player.components.builder.onBuild ~= nil then
+			oldonBuild = player.components.builder.onBuild
+			player.components.builder.onBuild = function(builder, prod)
+				oldonBuild(builder, prod)
+				HyperfolderOnBuild(builder, prod)
+			end
+		else
+			player.components.builder.onBuild = HyperfolderOnBuild
+		end
+	end
+end
+
 local OnEverySpawn = function(src, player)
 	if player.components.poisoned == nil then
 		player:AddComponent("poisoned")
