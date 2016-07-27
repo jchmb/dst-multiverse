ed3204444bec83ca50912e64120a4a2892e17199
Jochem <jchmb@users.noreply.github.com>
2016-07-27 13:26:06 +0200

    Update hyperfolder.lua

--- scripts/components/hyperfolder.lua
+++ scripts/components/hyperfolder.lua
@@ -1,15 +1,29 @@
 local Hyperfolder = Class(function(self, inst)
 	self.inst = inst
-	self.startPortal = nil
-	self.endPortal = nil
+	self.startPortalID = nil
+	self.startWorldID = nil
+	self.endPortalID = nil
+	self.endWorldID = nil
 end)
 
 function Hyperfolder:OnLoad(data)
-	return	
+	if data.startPortalID ~= nil and data.startWorldID ~= nil then
+		self.startPortalID = data.startPortalID
+		self.startWorldID = data.startWorldID
+	end
+	if data.endPortalID ~= nil and data.endWorldID ~= nil then
+		self.endPortalID = data.endPortalID
+		self.endWorldID = data.endWorldID
+	end
 end
 
 function Hyperfolder:OnSave()
-	return {}	
+	return {
+		startPortalID = self.startPortalID,
+		startWorldID = self.startWorldID,
+		endPortalID = self.endPortalID,
+		endWorldID = self.endWorldID,
+	}	
 end
 
 return Hyperfolder
