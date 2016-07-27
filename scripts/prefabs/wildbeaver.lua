0d86ff51bb6a41cad7ba431f99273c6408a99ad0
Jochem <jchmb@users.noreply.github.com>
2016-07-27 14:19:10 +0200

    Update wildbeaver.lua

--- scripts/prefabs/wildbeaver.lua
+++ scripts/prefabs/wildbeaver.lua
@@ -1,7 +1,121 @@
 local assets = {
-  
+    
 }
 
 local prefabs = {
-  
+    "log",
 }
+
+local function fn()
+    local inst = CreateEntity()
+    
+    inst.entity:AddTransform()
+    inst.entity:AddAnimState()
+    inst.entity:AddSoundEmitter()
+    inst.entity:AddDynamicShadow()
+    inst.entity:AddLightWatcher()
+    inst.entity:AddNetwork()
+    
+    MakeCharacterPhysics(inst, 50, .5)
+
+    inst.DynamicShadow:SetSize(1.5, .75)
+    inst.Transform:SetFourFaced()
+
+    inst:AddTag("character")
+    inst:AddTag("wildbeaver")
+    inst:AddTag("scarytoprey")
+    
+    inst.AnimState:SetBank("pigman")
+    inst.AnimState:PlayAnimation("idle_loop")
+    inst.AnimState:Hide("hat")
+    
+    --trader (from trader component) added to pristine state for optimization
+    inst:AddTag("trader")
+
+    --Sneak these into pristine state for optimization
+    inst:AddTag("_named")
+    
+    inst.entity:SetPristine()
+
+    if not TheWorld.ismastersim then
+        return inst
+    end
+    
+     --Remove these tags so that they can be added properly when replicating components below
+    inst:RemoveTag("_named")
+    
+    inst:AddComponent("locomotor") -- locomotor must be constructed before the stategraph
+    inst.components.locomotor.runspeed = TUNING.PIG_RUN_SPEED --5
+    inst.components.locomotor.walkspeed = TUNING.PIG_WALK_SPEED --3
+
+    inst:AddComponent("bloomer")
+    
+    inst:AddComponent("eater")
+    inst.components.eater:SetDiet({ FOODGROUP.WOODIE }, { FOODGROUP.WOODIE })
+    inst.components.eater:SetCanEatHorrible()
+    inst.components.eater:SetCanEatRaw()
+    inst.components.eater.strongstomach = true -- can eat monster meat!
+    inst.components.eater:SetOnEatFn(OnEat)
+    
+    inst:AddComponent("health")
+    inst:AddComponent("combat")
+    inst.components.combat.hiteffectsymbol = "pig_torso"
+
+    MakeMediumBurnableCharacter(inst, "pig_torso")
+
+    inst:AddComponent("named")
+    inst.components.named.possiblenames = STRINGS.PIGNAMES
+    inst.components.named:PickNewName()
+    
+     MakeHauntablePanic(inst)
+     
+     inst:AddComponent("follower")
+    inst.components.follower.maxfollowtime = TUNING.PIG_LOYALTY_MAXTIME
+    ------------------------------------------
+
+    inst:AddComponent("inventory")
+
+    ------------------------------------------
+
+    inst:AddComponent("lootdropper")
+
+    ------------------------------------------
+
+    inst:AddComponent("knownlocations")
+
+    ------------------------------------------
+
+    inst:AddComponent("trader")
+    inst.components.trader:SetAcceptTest(ShouldAcceptItem)
+    inst.components.trader.onaccept = OnGetItemFromPlayer
+    inst.components.trader.onrefuse = OnRefuseItem
+    inst.components.trader.deleteitemonaccept = false
+    
+    ------------------------------------------
+
+    inst:AddComponent("sanityaura")
+    inst.components.sanityaura.aurafn = CalcSanityAura
+
+    ------------------------------------------
+
+    inst:AddComponent("sleeper")
+
+    ------------------------------------------
+    MakeMediumFreezableCharacter(inst, "pig_torso")
+
+    ------------------------------------------
+
+    inst:AddComponent("inspectable")
+    inst.components.inspectable.getstatus = GetStatus
+    ------------------------------------------
+
+    inst.OnSave = OnSave
+    inst.OnLoad = OnLoad
+
+    inst:ListenForEvent("attacked", OnAttacked)
+    inst:ListenForEvent("newcombattarget", OnNewTarget)
+    
+    return inst
+end
+
+return Prefab("wildbeaver", fn, assets, prefabs)
