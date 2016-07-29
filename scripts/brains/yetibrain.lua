7a2d770164cc872f078ceaf1f1a3f32bb6deea19
Jochem <jchmb@users.noreply.github.com>
2016-07-29 11:23:20 +0200

    Create yetibrain.lua

---
+++ scripts/brains/yetibrain.lua
@@ -0,0 +1,39 @@
+require "behaviours/chaseandattack"
+require "behaviours/runaway"
+require "behaviours/wander"
+require "behaviours/doaction"
+require "behaviours/attackwall"
+require "behaviours/panic"
+require "behaviours/minperiod"
+require "giantutils"
+
+local SEE_DIST = 40
+
+local CHASE_DIST = 32
+local CHASE_TIME = 20
+
+local function GetWanderPos(inst)
+    return inst.components.knownlocations:GetLocation("spawnpoint")
+end
+
+local YetiBrain = Class(Brain, function(self, inst)
+    Brain._ctor(self, inst)
+end)
+
+function YetiBrain:OnStart()
+    local root =
+        PriorityNode(
+        {
+            AttackWall(self.inst),
+            ChaseAndAttack(self.inst, CHASE_TIME, CHASE_DIST),
+            Wander(self.inst, GetWanderPos, 30, {minwwwalktime = 10}),
+        },1)
+    
+    self.bt = BT(self.inst, root)
+end
+
+function YetiBrain:OnInitializationComplete()
+    self.inst.components.knownlocations:RememberLocation("spawnpoint", Point(self.inst.Transform:GetWorldPosition()))
+end
+
+return YetiBrain
