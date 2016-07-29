a5eb497bf46a468c514bba86cc54f066d9a958ba
Jochem <jchmb@users.noreply.github.com>
2016-07-29 15:29:38 +0200

    Update giant_bunnyman_spawner.lua

--- scripts/prefabs/giant_bunnyman_spawner.lua
+++ scripts/prefabs/giant_bunnyman_spawner.lua
@@ -4,24 +4,18 @@ local prefabs =
 }
 
 local function OnKilled(inst)
-    inst.components.timer:StartTimer("regen_dragonfly", TUNING.DRAGONFLY_RESPAWN_TIME)
+    inst.components.timer:StartTimer("regen_giant_bunnyman", TUNING.DRAGONFLY_RESPAWN_TIME)
 end
 
-local function GenerateNewDragon(inst)
+local function GenerateNewGiantBunnyman(inst)
     inst.components.childspawner:AddChildrenInside(1)
     inst.components.childspawner:StartSpawning()
 end
 
 local function ontimerdone(inst, data)
-    if data.name == "regen_dragonfly" then
-        GenerateNewDragon(inst)
+    if data.name == "regen_giant_bunnyman" then
+        GenerateNewGiantBunnyman(inst)
     end
-end
-
-local function onspawned(inst, child)
-    local x, y, z = child.Transform:GetWorldPosition()
-    child.Transform:SetPosition(x, 20, z)
-    child.sg:GoToState("land")
 end
 
 local function fn()
@@ -33,13 +27,13 @@ local function fn()
     inst:AddTag("CLASSIFIED")
 
     inst:AddComponent("childspawner")
-    inst.components.childspawner.childname = "dragonfly"
+    inst.components.childspawner.childname = "giant_bunnyman"
     inst.components.childspawner:SetMaxChildren(1)
     inst.components.childspawner:SetSpawnPeriod(TUNING.DRAGONFLY_SPAWN_TIME, 0)
     inst.components.childspawner.onchildkilledfn = OnKilled
     inst.components.childspawner:StartSpawning()
     inst.components.childspawner:StopRegen()
-    inst.components.childspawner:SetSpawnedFn(onspawned)
+    --inst.components.childspawner:SetSpawnedFn(onspawned)
 
     inst:AddComponent("timer")
     inst:ListenForEvent("timerdone", ontimerdone)
