88e443b933cf7077c92dae8f21041b4bf7c7e804
Jochem <jchmb@users.noreply.github.com>
2016-08-05 12:54:01 +0200

    Create piratebunnymanherd.lua

---
+++ scripts/prefabs/piratebunnymanherd.lua
@@ -0,0 +1,29 @@
+local prefabs = 
+{
+    "piratebunnyman",
+}
+
+local MAX_SIZE = 20
+
+local function fn()
+    local inst = CreateEntity()
+
+    inst.entity:AddTransform()
+    --[[Non-networked entity]]
+
+    inst:AddTag("herd")
+    --V2C: Don't use CLASSIFIED because herds use FindEntities on "herd" tag
+    inst:AddTag("NOBLOCK")
+    inst:AddTag("NOCLICK")
+
+    inst:AddComponent("herd")
+    inst.components.herd:SetMemberTag("piratebunnyman")
+    inst.components.herd:SetGatherRange(40)
+    inst.components.herd:SetUpdateRange(20)
+    inst.components.herd:SetMaxSize(MAX_SIZE)
+    inst.components.herd:SetOnEmptyFn(inst.Remove)
+
+    return inst
+end
+
+return Prefab("piratebunnymanherd", fn, nil, prefabs)
